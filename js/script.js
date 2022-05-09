481a2a55e6bedbbf5d3577a53b5c1fe32d5c3a57
Antu <antukhadem17@gmail.com>
2022-05-10 01:18:34 +0600

    scroll issue

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const slider = document.querySelector('.hero__slider');
 const prof__img = document.querySelector('.hero__profile');
 const hero__info = document.querySelector('.hero__info');
 
-gsap.from('.header', { duration: 1, y: '-100%', ease: Power2.easeInOut });
+gsap.from('.header', { duration: 1, y: '100%', ease: Power2.easeInOut });
 
-gsap.to(slider, { duration: 1, x: '120%', ease: Power2.easeIn });
+gsap.to(slider, { duration: 1, x: '100%', ease: Power2.easeIn });
 gsap.from(hero__info, { duration: 2, x: '-180%', ease: Power2.easeInOut });
 gsap.from(prof__img, { duration: 2, x: '180%', ease: Power2.easeInOut });
 
