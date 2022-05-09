55d561036c00d5dc7220bc96f88ef36abdee84d4
Antu <antukhadem17@gmail.com>
2022-05-10 01:25:56 +0600

    fixed scroll issue on hero section

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const hero__info = document.querySelector('.hero__info');
 
 gsap.from('.header', { duration: 1, y: '100%', ease: Power2.easeInOut });
 
-gsap.to(slider, { duration: 1, x: '100%', ease: Power2.easeIn });
+gsap.to(slider, { duration: 1, x: '120%', ease: Power2.easeIn });
 gsap.from(hero__info, { duration: 2, x: '-180%', ease: Power2.easeInOut });
 gsap.from(prof__img, { duration: 2, x: '180%', ease: Power2.easeInOut });
 
